2b61e9402e62b9f77464cfe9c45a961ee5a7646f
grayTurtle01 <the.correo@protonmail.com>
2022-05-14 16:34:05 -0500

    fixed bug on scores/:id

--- src/routes/scores.js
+++ src/routes/scores.js
@@ -18,8 +18,9 @@ router.get('/', (req, res) => {
 // //GET ONE SCORE 
   router.get('/:id', (req, res) => {
   
-    const {id} = req.params.id
-  
+    const { id } = req.params
+    console.log(id)
+
     var sqlcommand = 'SELECT * FROM TB_SCORE WHERE ID_SCORE = ?';
     mysqlConnection.query(sqlcommand,[id], (err, rows, fields) => {
       if(err) throw err;
@@ -30,21 +31,21 @@ router.get('/', (req, res) => {
   });
   
 //   //PUT SCORE  
-//   router.put('/Score/:id', (req,res) => {
+  router.put('/:id', (req,res) => {
   
-//     const {id} = req.params.id
+    const {id} = req.params.id
   
-//     let {puntuacion} = req.body
+    let {puntuacion} = req.body
   
-//     var sqlcommand = UPDATE TB_CLIENTE SET PUNTUACION  = '${puntuacion}' WHERE ID_CLIENTE = ${id} ;
+    var sqlcommand = `UPDATE TB_CLIENTE SET PUNTUACION  = ${puntuacion} WHERE ID_CLIENTE = ${id}` ;
   
-//     mysqlConnection.query(sqlcommand, (err, rows, fields) => {
-//       if(err) throw err;
-//       else {
-//         res.json({status: 'Score Actualizado'});
-//       }
-//     }); 
-//   })
+    mysqlConnection.query(sqlcommand, (err, rows, fields) => {
+      if(err) throw err;
+      else {
+        res.json({status: 'Score Actualizado'});
+      }
+    }); 
+  })
   
 //   //DELETE SCORE
 //   router.delete('/Score/:id',(req,res) =>{
