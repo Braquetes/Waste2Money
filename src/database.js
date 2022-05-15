09305ddc27a35f3e9c31c7e576d16be7b5dca813
grayTurtle01 <the.correo@protonmail.com>
2022-05-15 10:08:24 -0500

    azure environment variables

--- src/database.js
+++ src/database.js
@@ -1,12 +1,22 @@
 const mysql = require('mysql');
 
-const mysqlConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'WASTE2MONEYDB',
-  multipleStatements: true
+//~ const mysqlConnection = mysql.createConnection({
+  //~ host: 'localhost',
+  //~ user: 'root',
+  //~ password: 'password',
+  //~ database: 'WASTE2MONEYDB',
+  //~ multipleStatements: true
+//~ });
+
+var mysqlConnection = mysql.createConnection({
+  host: process.env.HOST, 
+  user: process.env.MYSQL_USER, 
+  password: process.env.PASSWORD, 
+  database: process.env.DATABASE, 
+  port: 3306
 });
+
+
 
 mysqlConnection.connect(function (err) {
   if (err) {
